e08886214df564dc2ba9aa2d43312cde6153f6dd
Nikolay21102 <95417145+Nikolay21102@users.noreply.github.com>
2021-12-06 15:41:41 +0300

    Добавляем в обработчик события код для кнопок Start и Cancel

--- WinBackgroundWorker/Form1.Designer.cs
+++ WinBackgroundWorker/Form1.Designer.cs
@@ -79,6 +79,7 @@ namespace WinBackgroundWorker
             this.button1.TabIndex = 4;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -88,6 +89,7 @@ namespace WinBackgroundWorker
             this.button2.TabIndex = 5;
             this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // backgroundWorker1
             // 
