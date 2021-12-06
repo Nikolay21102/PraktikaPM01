8696d21712a5e128322b284b026b35f7a2697b21
Nikolay21102 <95417145+Nikolay21102@users.noreply.github.com>
2021-12-06 16:04:39 +0300

    Добавляем в приложение методы и делегаты, приведенные в задании

--- WinAsynchDelegate/Form1.cs
+++ WinAsynchDelegate/Form1.cs
@@ -16,5 +16,30 @@ namespace WinAsynchDelegate
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void TimeConsumingMethod(int seconds)
+        {
+            for (int j = 1; j <= seconds; j++)
+                System.Threading.Thread.Sleep(1000);
+        }
+        private delegate void TimeConsumingMethodDelegate(int seconds);
+
+        public delegate void SetProgressDelegate(int val);
+        public void SetProgress(int val)
+        {
+            if (progressBar1.InvokeRequired)
+            {
+                SetProgressDelegate del = new SetProgressDelegate(SetProgress);
+                this.Invoke(del, new object[] { val });
+            }
+            else
+            {
+                progressBar1.Value = val;
+            }
+        }
     }
 }
